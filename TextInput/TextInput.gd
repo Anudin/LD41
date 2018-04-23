63cbd57d81e1bad3e88a4389c1af0a185e6e6799
Anudin <bastian.boehm-vhm@web.de>
2018-04-23 08:58:03 +0200

    Configure input to allow menues

--- TextInput/TextInput.gd
+++ TextInput/TextInput.gd
@@ -20,8 +20,7 @@ func _input(event):
 
 func keyboard_input(event):
 	if event.is_pressed() and not event.is_echo():
-		if (event.unicode >= "a".ord_at(0) and event.unicode <= "z".ord_at(0)) or \
-		(event.unicode >= "A".ord_at(0) and event.unicode <= "Z".ord_at(0)):
+		if is_alphabetic_char(event):
 			var typed_char = char(event.unicode)
 						
 			if $TextDisplay.text == DEFAULT_TEXT:
@@ -30,7 +29,7 @@ func keyboard_input(event):
 				$TextDisplay.text += typed_char
 		else:
 			if SUBMIT.has(event.scancode):
-				if not $TextDisplay.text == DEFAULT_TEXT:
+				if $TextDisplay.text != DEFAULT_TEXT:
 					var command = $TextDisplay.text.to_lower()
 					
 					command = Main.verify_command(command)
@@ -53,11 +52,15 @@ func keyboard_input(event):
 				else:
 					submit_queue()
 			elif DELETE.has(event.scancode):
+				if $TextDisplay.text == DEFAULT_TEXT:
+					queue.clear()
+					emit_signal("queue_updated", queue)
+				
 				$TextDisplay.text = DEFAULT_TEXT
-			elif event.scancode == KEY_ESCAPE:
-				queue.clear()
-				$TextDisplay.text = DEFAULT_TEXT
-				emit_signal("queue_updated", queue)
+
+func is_alphabetic_char(event):
+	return (event.unicode >= "a".ord_at(0) and event.unicode <= "z".ord_at(0)) or \
+		(event.unicode >= "A".ord_at(0) and event.unicode <= "Z".ord_at(0))
 
 func submit_queue():
 	emit_signal("text_command", queue.pop_front())
